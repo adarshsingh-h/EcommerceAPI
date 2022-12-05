9fe871703237de5642dd0ba935a5ca3cf8f831f5
adarshsingh-h <adarshsingh160798@gmail.com>
2022-12-05 20:02:42 +0530

    Added Comments in Routes

--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,16 @@ const express = require("express");
 const ProductController = require("../controllers/ProductController");
 const router = express.Router();
 
+//Get Products Route
 router.get("/", ProductController.get_all_products);
 
+//Create Products Route
 router.post("/create", ProductController.create_products);
 
+//Delete Product
 router.delete("/:id", ProductController.delete_product);
 
+//Update Quantity of a Product
 router.patch("/:id", ProductController.update_product);
 
 module.exports = router;
